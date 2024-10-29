ad1895f5c51929906a8d8851d6ae4afc8eb29677
taniasilvana <taniasilvana.lapalelo@wingscorp.com>
2024-10-29 17:07:25 +0700

    fix(project): serialize result from server to client component

--- app/admin/projects/page.tsx
+++ app/admin/projects/page.tsx
@@ -3,9 +3,22 @@ import { getAllProject } from "@/lib/actions/project.action";
 
 const Projects = async () => {
   const results = await getAllProject();
+  
+  // Ensure the data is serializable
+  const projects = results.map((project) => ({
+    id: project.id.toString(),
+    title: project.title,
+    description: project.description,
+    url: project.url,
+    codeUrl: project.codeUrl,
+    imageUrl: project.imageUrl,
+    isFeatured: project.isFeatured,
+  }));
+
+
   return (
     <>
-    <TableProjects elements={results}/>
+    <TableProjects elements={projects}/>
     </>
   );
 };
